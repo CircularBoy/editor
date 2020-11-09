857006385d845c5db23821a4bab7e0af41197737
CircularBoy <--yurets097@gmail.com>
2020-11-09 12:21:23 +0200

    Delete execCommand and do all on selection api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Panel msg="Hello world"/>
+    <div
+      id="edit"
+      contenteditable
+      @input="listener">
+      Change me
+<!--      <b>I am bold</b><span>hahah</span>-->
+    </div>
+    <button @click="selection">getSelection</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Panel from './components/Panel.vue'
 
 export default {
   name: 'App',
+  data: function() {
+    return {
+      someData: 'Hello kitty',
+      allData: []
+    }
+  },
+  mounted: function() {
+    this.allData = this.getObjects()
+  },
+  methods: {
+    listener: function(e) {
+      this.someData = e.target.innerText
+      this.allData = this.getObjects()
+      // console.log(this.getObjects())
+    },
+    selection: function () {
+      // console.log(window.getSelection())
+      let part = window.getSelection().getRangeAt(0).extractContents()
+      console.log(part)
+      // let edit = document.getElementById('edit')
+      // let range = new Range();
+      // range.setStart(edit, 0)
+      // range.setEnd(edit, 2)
+      // document.getSelection().addRange(range)
+    },
+    getObjects: function() {
+      let edit = document.getElementById('edit');
+      let arr = [];
+
+      [].forEach.call(edit.childNodes, function(node) {
+        function getStyle(node, styleProp) {
+          return node.nodeType === 3 ?
+            getComputedStyle(node.parentElement)[styleProp] :
+            getComputedStyle(node)[styleProp]
+        }
+
+        let obj = {
+          text: node.nodeType === 3 ? node.nodeValue : node.innerHTML,
+          fontSize: getStyle(node, 'fontSize'),
+          color: getStyle(node, 'color')
+        }
+
+        arr.push(obj)
+      });
+
+      return arr;
+    }
+  },
   components: {
-    HelloWorld
+    Panel
   }
 }
 </script>
---
+++ src/components/Panel.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+<!--    <button @click="incFont">Increase font</button>-->
+<!--    <button @click="decFont">Decrease font</button>-->
+    <p>
+      <button @click="desc">-</button>
+      <span>Change font({{currentSize}})</span>
+      <button @click="inc">+</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Panel',
+  props: {
+    edit: String
+  },
+  data: function() {
+    return {
+      currentSize: 3
+    }
+  },
+  methods: {
+    inc: function() {
+      this.currentSize = this.currentSize <= 7 ? this.currentSize += 1 : 7;
+      document.execCommand('fontSize', false, this.currentSize)
+    },
+    desc: function() {
+      this.currentSize = this.currentSize >= 1 ? this.currentSize -= 1 : 1;
+      document.execCommand('fontSize', false, this.currentSize)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
